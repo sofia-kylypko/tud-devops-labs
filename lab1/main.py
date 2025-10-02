c46821af27fa248b6d64007c720090a5e46326ac
sofia.kilipko <sofia.kilipko@gmail.com>
2025-10-02 13:10:42 +0100

    task for fisrt lab done

---
+++ lab1/main.py
@@ -0,0 +1,17 @@
+# add some code
+
+import datetime
+
+print("Hello, Git!")
+print("Learning about git diff.")
+print(f"Today is {datetime.date.today()}")
+
+# git diff 
+
+# git chekout -- filename 
+# to revert changes to that file
+
+# git diff 
+# see no changes were made / changes were removed
+
+# done
